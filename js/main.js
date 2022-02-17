78f8a06782e62a2854ecf71f8366031e3e1aed2b
Sakhavat <sakhavatra@code.edu.az>
2022-02-17 22:52:00 +0400

    section_2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function start_1() {
   
-    if (confirm('text') == true)
+    if (confirm("Secim et") == true)
      {
         document.getElementById("body").style.backgroundColor="black"
       }
@@ -9,4 +9,34 @@ function start_1() {
         document.getElementById("body").style.backgroundColor="red"
     }
       
+  }
+
+  function start_2() {
+    let age = prompt("User yaşını daxil etməlidir");
+    
+    if(isNaN(age))
+    {
+        alert("Ancaq rəqəm daxil edilməlidir !!!")
+        start_2();
+
+
+    }
+    else
+    {
+        if (age != null) 
+        {
+            if(age<18)
+            {
+                alert(" Diqqət !!! siz bu sehifəyə daxil ola bilmərsiniz")
+            }
+            else{
+                alert("Ugurlu giris")
+            }
+            
+        }
+        else{
+            alert("bos olmaz")
+        }
+    }
+    
   }
